956fb63e833488501a8122a61dfc5ed316275a50
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-21 00:09:49 -0600

    added some data into database tables

--- db/schema.sql
+++ db/schema.sql
@@ -22,7 +22,7 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT,
-manager_id INT,
 FOREIGN KEY(role_id) REFERENCES roles(id),
-FOREIGN KEY(manager_id) REFERENCES employees(id) ON DELETE SET NULL
+manager_id INT NULL,
+FOREIGN KEY(manager_id) REFERENCES employees(id)
 )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,18 @@
 USE employee_db;
 
 INSERT INTO departments(name)
-value ("Sales"),("HR"),("Service"),("Marketing"),("Customer Service"),("Parts");
+VALUES ("Sales"),("Service"),("Marketing"),("Parts");
 
+INSERT INTO roles (title,salary,departments_id)
+VALUES("sales-man",60000,1),("sales-manager",80000,1),
+("service-technician",50000,2),("service-manager",77777,2),
+("marketing-specialist",55000,3),("marketing-director",88888,3),
+("parts-driver",40000,4),("parts-advisor",75000,4);
 
-select all * from departments
+INSERT INTO employees(first_name,last_name,role_id,manager_id)
+VALUES("Dulcie","Lucinde",2,NULL),("Lance","Magda",4,NULL),("Irma","Ylli",6,NULL),("Matts","Oline",8,NULL),
+("Lea","Nita",1,1),("Virgilio","Noy",1,1),("Sohail","Laureen",1,1),
+("Domitian","Suse",3,2),("Tacita","Herleif",3,2),("Elon","Fortuna",3,2),
+("Murad","Sotos",5,3),("Roland","Nala",5,3),("Iagan","Oum",5,3),
+("Abenner","Marius",7,4);
+
