e2597a9fce8dab14d3efbe371944ded26ef35444
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-21 00:49:13 -0600

    adding the prompt in server.js

--- server.js
+++ server.js
@@ -2,9 +2,55 @@ const inquirer = require('inquirer');
 const { welcome, runQuery } = require('./db');
 
 
-const mainPage = async () => {
+const mainPage = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+    ])
 
-};
+        .then((answers) => {
+            const { choices } = answers;
 
-const opening = () => { welcome(); mainPage(); }
+            if (choices === "View all departments") {
+                showAllDepartments();
+            }
+
+            if (choices === "View all roles") {
+                showAllRoles();
+            }
+
+            if (choices === "View all employees") {
+                showAllEmployees();
+            }
+
+            if (choices === "Add a department") {
+                departmentAdding();
+            }
+
+            if (choices === "Add a role") {
+                roleAdding();
+            }
+
+            if (choices === 'Add an employee') {
+                employeeAdding();
+            }
+
+            if (choices === 'Update an employee role') {
+                employeeUpdate();
+            }
+        });
+}
+const opening = () => { console.log("Welcome to Employee Track system!"); welcome(); mainPage(); }
 opening();
