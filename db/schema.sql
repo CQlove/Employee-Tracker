6902ec590407c41acb06b904c4e7b4073c6b5547
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-20 21:15:49 -0600

    added sql file

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+id INT NOT NULL AUTO_INCREMENT,
+);
+
+
+CREATE TABLE roles (
+id INT NOT NULL AUTO_INCREMENT,
+);
+
+CREATE TABLE employees (
+id INT NOT NULL AUTO_INCREMENT,
+);
