b7791afe72c4e8af803de61a4b385b398772cb90
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-21 00:26:55 -0600

    db.js module added

--- db/db.js
+++ db/db.js
@@ -1,3 +1,29 @@
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASS,
+    database: 'employee_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected to the database.');
+});
+
+
+const runQuery = (sql, values) => {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, values, (err, results) => {
+            if (err) return reject(err);
+            resolve(results);
+        });
+    });
+};
+
+module.exports = {
+    runQuery
+};
